4b78bb4625447113f4f99ffbaad6163db7cdd9ab
Joe <ujbeszel@gmail.com>
2017-12-02 21:17:08 +0100

    Add "Woocommerce Add to cart Ajax for variable products"

--- exopite-wc-ajaxify/public/class-exopite-wc-ajaxify-public.php
+++ exopite-wc-ajaxify/public/class-exopite-wc-ajaxify-public.php
@@ -100,4 +100,93 @@ class Exopite_Wc_Ajaxify_Public {
 
 	}
 
+    public function add_to_cart_variable_add_section( $sections ) {
+
+        $sections['wc_ajax_add_to_cart_variable'] = __( 'WC Ajax for Variable Products', 'text-domain' );
+        return $sections;
+
+    }
+
+    public function add_to_cart_variable_all_settings( $settings, $current_section ) {
+
+        /**
+         * Check the current section is what we want
+         **/
+
+        if ( $current_section == 'wc_ajax_add_to_cart_variable' ) {
+
+            $settings_slider = array();
+
+            // Add Title to the Settings
+            $settings_slider[] = array( 'name' => __( 'WC Ajax for Variable Products Settings', 'text-domain' ), 'type' => 'title', 'desc' => __( 'The following options are used to configure WC Ajax for Variable Products', 'text-domain' ), 'id' => 'wc_ajax_add_to_cart_variable' );
+
+            // Add first checkbox option
+            $settings_slider[] = array(
+
+                'name'     => __( 'Add Selection option to Category Page', 'text-domain' ),
+                'desc_tip' => __( 'This will automatically insert variable selection options on product Category Archive Page', 'text-domain' ),
+                'id'       => 'wc_ajax_add_to_cart_variable_category_page',
+                'type'     => 'checkbox',
+                'css'      => 'min-width:300px;',
+                'desc'     => __( 'Enable Varition select option on Category Archive page', 'text-domain' ),
+
+            );
+
+            $settings_slider[] = array( 'type' => 'sectionend', 'id' => 'wc_ajax_add_to_cart_variable' );
+
+            return $settings_slider;
+
+        /**
+         * If not, return the standard settings
+         **/
+
+        } else {
+
+            return $settings;
+
+        }
+
+    }
+
+    public function add_to_cart_variable_rc_callback() {
+
+        ob_start();
+
+        $product_id = apply_filters( 'woocommerce_add_to_cart_product_id', absint( $_POST['product_id'] ) );
+        $quantity = empty( $_POST['quantity'] ) ? 1 : apply_filters( 'woocommerce_stock_amount', $_POST['quantity'] );
+        $variation_id = $_POST['variation_id'];
+        $variation  = $_POST['variation'];
+        $passed_validation = apply_filters( 'woocommerce_add_to_cart_validation', true, $product_id, $quantity );
+
+        if ( $passed_validation && WC()->cart->add_to_cart( $product_id, $quantity, $variation_id, $variation  ) ) {
+
+            do_action( 'woocommerce_ajax_added_to_cart', $product_id );
+
+            if ( get_option( 'woocommerce_cart_redirect_after_add' ) == 'yes' ) {
+
+                wc_add_to_cart_message( $product_id );
+
+            }
+
+            // Return fragments
+            WC_AJAX::get_refreshed_fragments();
+
+        } else {
+
+            $this->json_headers();
+
+            // If there was an error adding to the cart, redirect to the product page to show any errors
+            $data = array(
+                'error' => true,
+                'product_url' => apply_filters( 'woocommerce_cart_redirect_after_error', get_permalink( $product_id ), $product_id )
+                );
+            echo json_encode( $data );
+
+        }
+
+        die();
+
+    }
+
+
 }
